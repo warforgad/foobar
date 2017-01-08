18751fbe5df10f86116e5402bfcc9580ebd916bb
Or Karni <warforgad@gmail.com>
2017-01-08 18:12:37 +0200

    Added system test

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,9 @@ def test_foo():
     with my_app.test_client() as c:
         rv = c.get('/foo')
         assert rv.status_code == 200 and rv.data.decode() == foobar.foo()
+
+def test_bar():
+    with my_app.test_client() as c:
         rv = c.get('/bar')
         assert rv.status_code == 200 and rv.data.decode() == foobar.bar()
 
