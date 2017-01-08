c2e205b49e0d2a24966abb54de07c1c5a776456a
Or Karni <warforgad@gmail.com>
2017-01-08 17:36:42 +0200

    Added flask app with unit test

---
+++ foobar/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from . import foo, bar
+my_app = Flask(__name__)
+
+@my_app.route('/')
+def hello():
+    return 'Welcome!'
+
+@my_app.route('/foo')
+def get_foo():
+    return foo()
+
+@my_app.route('/bar')
+def get_bar():
+    return bar()
---
+++ tests/test_flask.py
@@ -0,0 +1,10 @@
+import foobar
+from foobar.app import my_app
+
+def test_foo():
+    with my_app.test_client() as c:
+        rv = c.get('/foo')
+        assert rv.status_code == 200 and rv.data.decode() == foobar.foo()
+        rv = c.get('/bar')
+        assert rv.status_code == 200 and rv.data.decode() == foobar.bar()
+
