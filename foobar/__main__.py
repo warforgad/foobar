74b94429006303fe52ffdc5a90f9d711cd877205
Or Karni <warforgad@gmail.com>
2017-01-09 14:02:08 +0200

    Made tests work with the docker container

--- foobar/__main__.py
+++ foobar/__main__.py
@@ -1,3 +1,3 @@
 if __name__ == '__main__':
     from . import app
-    app.my_app.run(host='0.0.0.0')
+    app.my_app.run(host='0.0.0.0', port=80)
--- tests/test_system.py
+++ tests/test_system.py
@@ -2,37 +2,14 @@ import foobar, pytest, requests, socket
 from multiprocessing import Process
 from foobar.app import my_app
 
-ip = '0.0.0.0'
-port = '5000'
+ip = '127.0.0.1'
+port = '8000'
 url = 'http://{}:{}/'.format(ip, port)
 
-def server_main():
-    my_app.run(host=ip)        
-
-def wait_for_server():
-    while True:
-        try:
-            s = socket.socket()
-            s.connect(('127.0.0.1', int(port)))
-            break
-        except Exception:
-            pass
-
-@pytest.fixture(scope='module')
-def server():
-    server_process = Process(target=server_main)
-    server_process.start()
-    wait_for_server()
-    yield server_process
-
-    #teardown
-    server_process.terminate()
-    server_process.join()
-
-def test_foo(server):
+def test_foo():
     r = requests.get(url + 'foo')
     assert r.status_code == 200 and r.text == 'foo'
 
-def test_bar(server):
+def test_bar():
     r = requests.get(url + 'bar')
     assert r.status_code == 200 and r.text == 'bar'
