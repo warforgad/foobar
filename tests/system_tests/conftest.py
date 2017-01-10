2d91d41acc525c14c51a9faf0fec94a214f52cd1
Or Karni <warforgad@gmail.com>
2017-01-10 10:57:15 +0200

    Fixed tests

--- tests/system_tests/conftest.py
+++ tests/system_tests/conftest.py
@@ -1,3 +1,3 @@
 import sys, os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.dirname(os.path.abspath(__file__)))))
